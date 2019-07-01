d83d4ef62381ab1d476d32a742956b7b7da0b776
五十嵐 駿介 <igarashi@aizulab.com>
2019-07-01 09:32:31 +0900

    モジュール分割修正

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,79 @@
-import Browser
+module Main exposing (Model, Msg(..), Todo, init, main)
 
-import Model exposing (init, Model, Msg(..))
-import Update exposing (update)
-import View exposing (view)
+import Browser
+import Html exposing (..)
+import Html.Events exposing (onClick)
+
 
 main : Program () Model Msg
 main =
-  Browser.sandbox
-  { init = init
-  , update = update
-  , view = view
-  }
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
+
+
+
+-- Model
+
+
+type Msg
+    = Add
+
+
+type alias Todo =
+    { title : String
+    , description : String
+    }
+
+
+type alias Model =
+    { todos : List Todo }
+
+
+init : Model
+init =
+    { todos = [] }
+
+
+
+-- Update
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Add ->
+            { model | todos = Todo "Todoタイトル" "Todo詳細" :: model.todos }
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ viewTable model.todos
+        , button [ onClick Add ] [ text "Add" ]
+        ]
+
+
+viewTable : List Todo -> Html Msg
+viewTable todos =
+    table []
+        [ thead []
+            [ th [] [ text "Title" ]
+            , th [] [ text "Description" ]
+            ]
+        , tbody [] (List.map viewTr todos)
+        ]
+
+
+viewTr : Todo -> Html Msg
+viewTr todo =
+    tr []
+        [ td [] [ text todo.title ]
+        , td [] [ text todo.description ]
+        ]
--- src/Update.elm
+++
@@ -1,9 +0,0 @@
-module Update exposing (..)
-
-import Model exposing (Model, Msg(..), Todo)
-
-update : Msg -> Model -> Model
-update msg model =
-  case msg of
-    Add ->
-      { model | todos = ( Todo "Todoタイトル" "Todo詳細" ) :: model.todos }
